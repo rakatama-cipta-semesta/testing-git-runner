5861979da746ba08c453f9fbe55391fbfbec8c69
Jefri Herdi Triyanto <jefripunza@gmail.com>
2022-11-05 01:37:20 +0700

    test 2

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const express = require("express");
 const app = express(),
   port = 8080;
 app.listen(port, () => console.log(`Server listen at ${port}`));
+app.use(require("morgan")("combined"))
 app.all("*", (req, res) => res.send("hello world!"));
